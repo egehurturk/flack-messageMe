a3438f665ca1ee64906434d4f230c2e49e7e5592
Ege Hurturk <ege.hurturk@gmail.com>
2020-05-10 00:31:26 +0300

    Channel list, display made asynchronous

--- application.py
+++ application.py
@@ -9,17 +9,26 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = ["general", "random"]
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
 
+@socketio.on('get channels')
+def getChannels():
+    emit('channel list', {"channels":channels}, broadcast=True)
 
 
 @socketio.on("channel created")
 def channel(data):
     channel = data["channel"]
-    emit("show channel", {"channelName":channel}, broadcast=True)
+    if channel in channels:
+        emit('channel exists error', {"channel":channel}, broadcast=True)
+    else:
+        channels.append(channel)
+        emit('channel created', {}, broadcast=True)
+        emit('channel list', {"channels":channels}, broadcast=True)
 
 
 
